743cdff20158d0edfd103827ff7ac564e8d85c67
nehru35 <sonehru@gmail.com>
2022-06-03 19:48:27 +0100

    Solved the fifth exercise

---
+++ 05.js
@@ -0,0 +1,10 @@
+/*  Exercise 5:
+    -----------
+    Write a JavaScript program to create a new string adding "New!" in front of a given string. If the given string begins with "New!" already then return the original string.
+*/
+
+const addNew = (str) => 
+    str.indexOf('New') === 0 ? str : `New! ${str}`;
+
+
+console.log(addNew('N Offers!'))
